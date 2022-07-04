51c682435cb5afe9d490918069872305891355bb
Dansando8 <d.sandoval@vertciel.ca>
2022-07-04 18:46:42 -0400

    Order page under construction

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import { graphql } from 'gatsby';
+import React, { useState } from 'react';
+import Img from 'gatsby-image';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
+
+  const pizzas = data.pizzas.nodes;
+
   return (
-    <>
+    <div>
       <SEO title="Order a Pizza!" />
-      <p>Hey! I'm the Order page</p>
-    </>
+      <form>
+        <fieldset>
+          <legend>Your info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+          {pizzas.map((pizza) => (
+            <div>
+              <Img
+                fluid={pizza.image.asset.fluid}
+                width="50"
+                height="50"
+                alt={pizza.name}
+              />
+              <h2>{pizza.name}</h2>
+            </div>
+            <div></div>
+          ))}
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
+    </div>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
